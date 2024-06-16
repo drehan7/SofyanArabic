520bfd7692097b97ab098796955f0f6112c4992e
Dawud Rehan <d.rehan7@gmail.com>
2024-06-16 15:18:10 -0400

    contactpage, add pfp

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './Footer.css';
 import { Link } from 'react-router-dom';
 
+const LINKS = {
+    "linkedin": "https://www.linkedin.com/in/soufiane-el-mouaffaq-0750101b6?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    "ig": "https://www.instagram.com/soufiane.arabic?igsh=b2toOTR1YnJpZm9w",
+    "twitter": "https://x.com/sofyan_arabic?t=gbzspmlQMruZKjSOycQiLA&s=09",
+};
+
 function Footer() {
   return (
     <div className='footer-container'>
@@ -13,50 +19,32 @@ function Footer() {
             </Link>
           </div>
           <small className='website-rights'>SoufianeArabic © 2024</small>
-        {/*
           <div className='social-icons'>
             <Link
-              className='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i className='fab fa-facebook-f' />
-            </Link>
-            <Link
               className='social-icon-link instagram'
-              to='/'
+              to={{ pathname: LINKS["ig"] }}
               target='_blank'
               aria-label='Instagram'
             >
               <i className='fab fa-instagram' />
             </Link>
             <Link
-              className='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <i className='fab fa-youtube' />
-            </Link>
-            <Link
               className='social-icon-link twitter'
-              to='/'
+              to={{ pathname: LINKS["twitter"] }}
               target='_blank'
               aria-label='Twitter'
             >
               <i className='fab fa-twitter' />
             </Link>
             <Link
-              className='social-icon-link twitter'
-              to='/'
+              className='social-icon-link linkedin'
+              to={{ pathname: LINKS["linkedin"] }}
               target='_blank'
               aria-label='LinkedIn'
             >
               <i className='fab fa-linkedin' />
             </Link>
           </div>
-        */}
         </div>
       </section>
     </div>
